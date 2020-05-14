afc58895d3a6c727a53eed03ab0a5df9beaadaed
sa6kok <sa6kok@abv.bg>
2020-05-14 12:07:02 +0200

    index fixed

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
     children:
       [
         {
-          path: '',
+          path: 'index',
           component: IndexComponent,
           pathMatch: 'full',
         },
